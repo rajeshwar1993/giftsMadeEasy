0b8a3224654d72e621976aeb3779dc456255bf2c
Rajeshwar Rudra <rajeshwar1993@gmail.com>
2021-12-29 23:25:33 +0530

    HomePage redo

--- public_front_end/src/pages/index.tsx
+++ public_front_end/src/pages/index.tsx
@@ -6,37 +6,36 @@ import AllPages from '../core_custom_mixer/pageFormats';
 import { PageProps as Props } from '../core/pageFormats/types';
 import { DATA_SOURCE } from '../core_custom_mixer/app_config';
 
-const Home: NextPage<Props> = ({ headerData, sections }) => {
+const Home: NextPage<Props> = () => {
   const { SectionedPages } = AllPages;
   return (
     <div>
       <Head>
-        <title>{headerData.title}</title>
-        <meta name='description' content={headerData.metaDesc} />
-        <link rel='icon' href={headerData.fav || '/favicon.ico'} />
+        <title>Gifts Made Easy</title>
+        <meta name='description' content={'Meta description'} />
+        <link rel='icon' href={'/favicon.ico'} />
       </Head>
-      <SectionedPages sections={sections} />
     </div>
   );
 };
 
-export const getStaticProps: GetStaticProps<Props> = async context => {
-  // must be async
-  console.log(context);
+// export const getStaticProps: GetStaticProps<Props> = async context => {
+//   // must be async
+//   console.log(context);
 
-  let headerData, sections;
+//   let headerData, sections;
 
-  let fetchedData = await fetch(
-    `http://localhost:3000/api/${DATA_SOURCE.toString()}/data/homePage`
-  );
-  fetchedData = await fetchedData.json();
-  let parsedData = getDataParser(DATA_SOURCE, fetchedData);
-  headerData = parsedData.headerData;
-  sections = parsedData.sections;
+//   let fetchedData = await fetch(
+//     `http://localhost:3000/api/${DATA_SOURCE.toString()}/data/homePage`
+//   );
+//   fetchedData = await fetchedData.json();
+//   let parsedData = getDataParser(DATA_SOURCE, fetchedData);
+//   headerData = parsedData.headerData;
+//   sections = parsedData.sections;
 
-  return {
-    props: { headerData, sections }
-  };
-};
+//   return {
+//     props: { headerData, sections }
+//   };
+// };
 
 export default Home;
