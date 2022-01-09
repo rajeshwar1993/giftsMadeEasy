ed4af5de0207e5d0d9890bfc7ecb6b2261fa1a71
Rajeshwar Rudra <rajeshwar1993@gmail.com>
2022-01-09 19:00:51 +0530

    Removing old function folder

--- firebase/functions/index.js
+++
@@ -1,9 +0,0 @@
-// The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
-const { handleAddOrRemoveToCirle } = require('./src/circleFuncs');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.handleAddOrRemoveToCirle = handleAddOrRemoveToCirle;
