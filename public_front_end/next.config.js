6b226c17b0617143c7059346f2a12b478fb70e4d
Rajeshwar Rudra <rajeshwar1993@gmail.com>
2022-02-03 23:47:47 +0530

    removed the bundler

--- public_front_end/next.config.js
+++ public_front_end/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: true
-});
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: true
+// });
 
-module.exports = withBundleAnalyzer({
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['m.media-amazon.com', 'firebasestorage.googleapis.com']
   }
-});
+};
