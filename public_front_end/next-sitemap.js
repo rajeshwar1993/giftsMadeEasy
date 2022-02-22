6e0e8fc374577a1a7a5446dbc64698c06a5afdc2
Rajeshwar Rudra <rajeshwar1993@gmail.com>
2022-02-22 12:18:09 +0530

    sitemap generation

--- public_front_end/next-sitemap.js
+++
@@ -1,7 +0,0 @@
-// TODO needs config to add dynamic products link to sitemap
-
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'http://localhost:3000',
-  generateRobotsTxt: true // (optional)
-  // ...other options
-};
---
+++ public_front_end/sitemap-config.js
@@ -0,0 +1,25 @@
+// TODO needs config to add dynamic products link to sitemap
+
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
+  generateRobotsTxt: true, // (optional)
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', disallow: '/admin' },
+      { userAgent: '*', disallow: '/admin/*' },
+      { userAgent: '*', disallow: '/profile' },
+      { userAgent: '*', disallow: '/profile/*' },
+      { userAgent: '*', disallow: '/products' },
+      { userAgent: '*', disallow: '/products/*' },
+      { userAgent: '*', allow: '/' }
+    ]
+  },
+  exclude: [
+    '/admin',
+    '/admin/*',
+    '/profile',
+    '/profile/*',
+    '/products',
+    '/products/*'
+  ]
+};
